5be7915f01b59a3fde937539e2f84f5f22ce56ff
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2020-11-20 18:55:17 +0100

    Update: sqrt(x)

---
+++ 69. Sqrt(x)/solution.ts
@@ -0,0 +1,11 @@
+function mySqrt(x: number): number {
+    let resPrev: number = 1;
+    let epsilon: number = 1;
+    let resCurr: number;
+    while (epsilon > 1e-6) {
+        resCurr = resPrev + (x - resPrev * resPrev) / (2 * resPrev);
+        epsilon = Math.abs(resCurr - resPrev);
+        resPrev = resCurr;
+    }
+    return Math.floor(resPrev);
+};
