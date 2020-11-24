635500f346184c7b22359873f9e30302bca816a2
BASARANOMO <39518094+BASARANOMO@users.noreply.github.com>
2020-11-24 17:13:18 +0100

    Update: add generated js

--- GeneratedJS/solutions/Easy/344. Reverse String/solution1.js
+++ GeneratedJS/solutions/Easy/344. Reverse String/solution1.js
@@ -1,1 +1,18 @@
 "use strict";
+/**
+ Do not return anything, modify s in-place instead.
+ */
+// Naive method
+function reverseString(s) {
+    s = swap(s, 0, s.length - 1);
+}
+;
+function swap(s, left, right) {
+    if (left < right) {
+        var temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        s = swap(s, left + 1, right - 1);
+    }
+    return s;
+}
